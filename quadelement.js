ac52a7f1140937bcc218cf3e9f42604001595d71
Fragrag <haryo.sukmawanto@gmail.com>
2018-03-15 21:27:39 +0100

    Further work on dynamic colors and patterned changes (Not much progress on either)

--- quadelement.js
+++ quadelement.js
@@ -3,7 +3,7 @@
  The QuadElement will be defined by the following elements:
 	width, height
 	A drawn quad
-	FillColor,
+	FillquadColor,
 	StrokeThickness
 	
 	TODO: Define quad formula 
@@ -25,15 +25,25 @@ class QuadElement {
 		this.x = _x;
 		this.y = _y;
 		
+		// this.r = 128;
+		// this.g = 128;
+		// this.b = 128;
+		
+		this.QuadColor = {
+			r: 128,
+			g: 128,
+			b: 128
+		};
+		
 	}
 	
-	DrawQuad(r, g, b) {
+	DrawQuad() {
 		
 		var originX = this.x * this.width;
 		var originY = this.y * this.height;
 		
 		strokeWeight(.5);
-		fill(r, g, b);
+		fill(this.QuadColor.r, this.QuadColor.g, this.QuadColor.b);
 		
 		quad(
 			originX, originY,
@@ -41,6 +51,14 @@ class QuadElement {
 			(originX + (this.width + this.height)), (originY + this.height),
 			(originX + this.height), (originY + this.height)
 		);
+		
+	}
+	
+	SetQuadColor(_r, _g, _b){
+		
+		this.QuadColor.r = _r;
+		this.QuadColor.g = _g;
+		this.QuadColor.b = _b;
 		
 	}
 	
--- sketch.js
+++ sketch.js
@@ -1,31 +1,33 @@
 var quadArray = [];
-var rows = 24;
-var columns = 24;
+var rows = 12;
+var columns = 12;
 var quadHeight = 50;
 var quadWidth = 25;
 
 function setup() {
 	
 	background(128);
-	createCanvas(500, 500);
+	createCanvas(1000, 1000);
+	
+	//translate(-(quadWidth*2), 0);
 	
 	FillQuadArray(rows, columns, quadHeight, quadWidth, quadArray);
+
+	DrawQuadArray(quadArray);
 	
 }
 
 function draw() {
-	translate(-(quadWidth*2), 0);
-	DrawQuadArray(quadArray, 128, 150, 150);
-	
+
 	ColorABQuadArray(quadArray);
 }
 
 // Fill an initiated array with QuadElements 
 function FillQuadArray(_rows, _columns, _quadHeight, _quadWidth, _arrayVariable) {
 	
-	for (var i = 0; i < _rows; i++) {
+	for (var i = 0; i <=_rows; i++) {
 		_arrayVariable.push([0]);
-		for (var j = 0; j < _columns; j++) {
+		for (var j = 0; j <=_columns; j++) {
 			_arrayVariable[i][j] = new QuadElement(_quadHeight, _quadWidth, i, j);	
 		}
 	}
@@ -33,11 +35,11 @@ function FillQuadArray(_rows, _columns, _quadHeight, _quadWidth, _arrayVariable)
 }
 
 // Iterate through each element in QuadArray and call DrawQuad function
-function DrawQuadArray(_arrayVariable, r, g, b) {
+function DrawQuadArray(_arrayVariable) {
 	
 	for(var i = 0; i < _arrayVariable.length; i++){
-		for (var j = 0; j < _arrayVariable[i].length; j++){
-			_arrayVariable[i][j].DrawQuad(r, g, b);
+		for (var j = 0; j < _arrayVariable[i].length - 1; j++){
+			_arrayVariable[i][j].DrawQuad();
 		}
 	}
 	
@@ -47,7 +49,8 @@ function ColorABQuadArray(_arrayVariable) {
 	
 	for(var i = 0; i < _arrayVariable.length; i++){
 		for (var j = 0; j < _arrayVariable[i].length; j++) {
-			_arrayVariable[i][j].DrawQuad(128, 64, 64);
+			_arrayVariable[i][j].SetQuadColor(128, 64, 64);
+			_arrayVariable[i][j].DrawQuad();
 		}
 	}
 	
