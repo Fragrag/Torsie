cda77a246dfe4c9c10c5c03e2dc0e6c788a1ed61
Fragrag <haryo.sukmawanto@gmail.com>
2018-03-29 16:17:19 +0200

    - Added JQuery - Created vertical layout, will switch to horizontal

--- src/sketch.js
+++ src/sketch.js
@@ -13,9 +13,11 @@ let quadHeight = 15;
 let quadWidth = 15;
 let canvasHeight = 240
 let canvasWidth = 240;
-let canvasColor = 200;
+let canvasColor = 255;
 
-let canvasCentralPosX;
+let canvasCentralPosX = ($(window).width()/2) - (canvasWidth/2);
+let canvasLeftPosX = ($(window).width()/2) - (canvasWidth*2);
+let canvasRightPosX = ($(window).width()/2) + (canvasWidth);
 
 // GLOBAL COLOUR SETTINGS
 let QAC;
@@ -27,37 +29,37 @@ let color5;
 let color6;
 
 // This function sets up and returns a new Canvas object for a TorsieArray
-function TorsieInstance(CanvasPosX, CanvasPosY, CanvasPattern) {
+function TorsieInstance(InstanceName, CanvasPosX, CanvasPosY, CanvasPattern) {
 	
 	let CanvasSetup = function(sketch) {
 		
 		let color1 = 0;
 		
 		sketch.setup = function() {
-			let cnv = sketch.createCanvas(canvasWidth, canvasHeight);
+			var cnv = sketch.createCanvas(canvasWidth, canvasHeight);
 			cnv.position(CanvasPosX, CanvasPosY);
+			
 			sketch.background(canvasColor);
 			sketch.quads = new QuadArray(rows, columns, quadHeight, quadWidth, sketch);
 			sketch.quads.SetupQuadArray();
 		}
 
 		sketch.draw = function() {
+			
 			sketch.translate(-quadWidth*4, 0);
 			sketch.quads.DrawQuadArray();
 			CanvasPattern();
 		}
 	}
 	
-	return new p5(CanvasSetup);
+	return new p5(CanvasSetup, InstanceName);
 }
 
 function setup() {
-	
 	QAC = new QuadArrayController(null, 600);
+	createCanvas(windowWidth, windowHeight);
+	background(canvasColor);
 	
-	
-	
-
 }
 
 function draw() {
@@ -69,21 +71,51 @@ function draw() {
 	color4 = QAC.GetRGB(QAC.Color4.value());
 	color5 = QAC.GetRGB(QAC.Color5.value());
 	color6 = QAC.GetRGB(QAC.Color6.value());
-	
-	// Setting up responsive window settings
-	createCanvas(windowWidth, windowHeight);
-	background(canvasColor);
-	let canvasCentralPosX = (windowWidth/2) - (canvasWidth/2);
 }
 
 
 
-
-let CanvasInstance1 = TorsieInstance(canvasCentralPosX, 50, 
-									function(){CanvasInstance1.quads.SetPatternAB(color1.r, color1.g, color1.b,
+// Central column
+let CanvasInstanceAB = TorsieInstance("PatternAB", canvasCentralPosX, 25, 
+									function(){CanvasInstanceAB.quads.SetPatternAB(color1.r, color1.g, color1.b,
 																				  color2.r, color2.g, color2.b)});
-																						   
-let CanvasInstance2 = TorsieInstance(300, 300, 
-									function(){CanvasInstance2.quads.SetPatternABBC(color1.r, color1.g, color1.b,
+
+
+// Left column
+let CanvasInstanceABBC = TorsieInstance("PatternABBC", canvasLeftPosX, (canvasHeight+100), 
+									function(){CanvasInstanceABBC.quads.SetPatternABBC(color1.r, color1.g, color1.b,
 																					color2.r, color2.g, color2.b,
 																					color3.r, color3.g, color3.b)});
+
+let CanvasInstanceADBC = TorsieInstance("PatternADBC", canvasLeftPosX, (canvasHeight*2+150), 
+									function(){CanvasInstanceADBC.quads.SetPatternADBC(color1.r, color1.g, color1.b,
+																					color2.r, color2.g, color2.b,
+																					color3.r, color3.g, color3.b,
+																					color4.r, color4.g, color4.b)});
+
+// Right column
+let CanvasInstanceCAB = TorsieInstance("PatternCAB", canvasRightPosX, (canvasHeight+100), 
+									function(){CanvasInstanceCAB.quads.SetPatternCAB(color1.r, color1.g, color1.b,
+																					color2.r, color2.g, color2.b,
+																					color3.r, color3.g, color3.b)});
+
+let CanvasInstanceCABDAB = TorsieInstance("PatternCABDAB", canvasRightPosX, (canvasHeight*2+150), 
+									function(){CanvasInstanceCABDAB.quads.SetPatternCAB(color1.r, color1.g, color1.b,
+																						color2.r, color2.g, color2.b,
+																						color3.r, color3.g, color3.b,
+																						color4.r, color4.g, color4.b)});
+
+let CanvasInstanceCABDEB = TorsieInstance("PatternCABDEB", canvasRightPosX, (canvasHeight*3+200), 
+									function(){CanvasInstanceCABDEB.quads.SetPatternCABDEB(color1.r, color1.g, color1.b,
+																						   color2.r, color2.g, color2.b,
+																						   color3.r, color3.g, color3.b,
+																						   color4.r, color4.g, color4.b,
+																						   color5.r, color5.g, color5.b)});
+
+let CanvasInstanceCABDEF = TorsieInstance("PatternCABDEB", canvasRightPosX, (canvasHeight*4+250), 
+									function(){CanvasInstanceCABDEF.quads.SetPatternCABDEF(color1.r, color1.g, color1.b,
+																						   color2.r, color2.g, color2.b,
+																						   color3.r, color3.g, color3.b,
+																						   color4.r, color4.g, color4.b,
+																						   color5.r, color5.g, color5.b,
+																						   color6.r, color6.g, color6.b)});
