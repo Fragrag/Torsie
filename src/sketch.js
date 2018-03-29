28c4d7b547fbca4fa2e784932bc8e58abbc14661
Fragrag <haryo.sukmawanto@gmail.com>
2018-03-29 14:31:45 +0200

    - Started work on layout on the page.

--- src/sketch.js
+++ src/sketch.js
@@ -11,9 +11,11 @@ let rows = 63;
 let columns = 63;
 let quadHeight = 15;
 let quadWidth = 15;
-let canvasHeight = 180
-let canvasWidth = 180;
+let canvasHeight = 240
+let canvasWidth = 240;
 let canvasColor = 200;
+
+let canvasCentralPosX;
 
 // GLOBAL COLOUR SETTINGS
 let QAC;
@@ -50,14 +52,16 @@ function TorsieInstance(CanvasPosX, CanvasPosY, CanvasPattern) {
 }
 
 function setup() {
-	createCanvas(600, 600);
-	background(canvasColor);
+	
 	QAC = new QuadArrayController(null, 600);
+	
+	
+	
 
 }
 
 function draw() {
-	translate(-quadWidth*4, 0);
+	//translate(-quadWidth*4, 0);
 	
 	color1 = QAC.GetRGB(QAC.Color1.value());
 	color2 = QAC.GetRGB(QAC.Color2.value());
@@ -65,7 +69,21 @@ function draw() {
 	color4 = QAC.GetRGB(QAC.Color4.value());
 	color5 = QAC.GetRGB(QAC.Color5.value());
 	color6 = QAC.GetRGB(QAC.Color6.value());
+	
+	// Setting up responsive window settings
+	createCanvas(windowWidth, windowHeight);
+	background(canvasColor);
+	let canvasCentralPosX = (windowWidth/2) - (canvasWidth/2);
 }
 
-let CanvasInstance1 = TorsieInstance(50, 50, function(){CanvasInstance1.quads.SetPatternAB(color1.r, color1.g, color1.b,
-																						   color2.r, color2.g, color2.b)});
+
+
+
+let CanvasInstance1 = TorsieInstance(canvasCentralPosX, 50, 
+									function(){CanvasInstance1.quads.SetPatternAB(color1.r, color1.g, color1.b,
+																				  color2.r, color2.g, color2.b)});
+																						   
+let CanvasInstance2 = TorsieInstance(300, 300, 
+									function(){CanvasInstance2.quads.SetPatternABBC(color1.r, color1.g, color1.b,
+																					color2.r, color2.g, color2.b,
+																					color3.r, color3.g, color3.b)});
